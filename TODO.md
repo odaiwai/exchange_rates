c959c7ad10a7f18d8ae98a57a40198b5323f0ec7
Dave O'Brien <github@diaspoir.net>
2023-05-11 14:44:06 +0800

    updated the standard query and moved the time series out of the repo.

---
+++ TODO.md
@@ -0,0 +1,8 @@
+# TODO:
+
+Task to be underetaken in the future
+1. infill the earlier dates with: 
+
+    for year in `seq 2006 2015`; do ./get_exchange_rates.py $year-12-31 365; done
+    
+
--- query.sql
+++ query.sql
@@ -1,7 +1,11 @@
---Query to get a list of rates 
-Select HKD.date, 1 as HKD, USD.HKD, AUD.HKD, EUR.HKD, GBP.HKD from HKD 
-        join USD using(timestamp) 
-        join aud using(timestamp) 
-        join eur using(timestamp) 
-        join gbp using(timestamp) 
-        order by timestamp;
+--Query to get a list of rates
+Select  (substr(timestamp,1,4) || '-' ||
+         substr(timestamp,5,2) || '-' ||
+         substr(timestamp,7,2)) as Date,
+        1 as HKD, USD.HKD as USD, AUD.HKD as AUD, EUR.HKD as EUR, GBP.HKD as GBP from HKD 
+    join USD using(timestamp) 
+    join aud using(timestamp) 
+    join eur using(timestamp) 
+    join gbp using(timestamp)
+    WHERE timestamp > 20120000
+    order by timestamp;
